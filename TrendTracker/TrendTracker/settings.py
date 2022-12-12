f4fcaf22a458a9b962bd8cb76f534b2cefb886c1
clydejallorina <clydejallorina@gmail.com>
2022-12-12 21:01:11 +0800

    YES IT FINALLY WORKS

--- TrendTracker/TrendTracker/settings.py
+++ TrendTracker/TrendTracker/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-f5!g-m(&fjkq!g56bochpqqm9^tptc6f5&yq0#r+^#htovh40$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "trendtracker.com", ".trendtracker.com"]
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'TTBackend',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'TrendTracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("DB_NAME", default="TootTally"),
+        'USER': os.getenv("DB_USER", default="toottallyweb"),
+        'PASSWORD': os.getenv("DB_PASS", default="thisisarandompassword"),
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
