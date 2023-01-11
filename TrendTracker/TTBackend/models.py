ef391e3d4529b4958157189edd45cde46dd4b4e1
clydejallorina <clydejallorina@gmail.com>
2023-01-11 13:01:10 +0800

    api actually sends out api data now

--- TrendTracker/TTBackend/models.py
+++ TrendTracker/TTBackend/models.py
@@ -5,9 +5,13 @@ from django.utils import timezone
 # Create your models here.
 class TiktokUser(models.Model):
     username = models.CharField(max_length=128, default="")
-    url = models.URLField(max_length=256)
+    tiktok_id = models.BigIntegerField(default=0)
+    points = models.BigIntegerField(default=0)
+    positivity = models.CharField(max_length=16, default="neutral")
+    url = models.URLField(max_length=256, default="")
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.CharField(max_length=2048)
     url = models.URLField(max_length=256)
+    positivity = models.CharField(max_length=16, default="neutral")
--- TrendTracker/TTBackend/views.py
+++ TrendTracker/TTBackend/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from .models import *
+import json
 
 # Create your views here.
 
 ### DUMMY API VIEWS, REMOVE THESE WHEN WE ACTUALLY HAVE PROPER VIEWS
-def product_rankings():
+def product_rankings(request):
     test_rankings = [{"1": {"name": "TestProduct", "url": "http://testurl.com/"}}, {"2": {"name": "TestProduct2", "url": "http://testurl.com/"}}]
     return HttpResponse(str(test_rankings))
 
-def user_rankings():
-    test_rankings = [{"1": {"name": "TestUser", "url": "https://tiktok.com/"}}, {"2": {"name": "TestUser2", "url": "https://tiktok.com/"}}]
-    return HttpResponse(str(test_rankings))
+def user_rankings(request):
+    rankings = TiktokUser.objects.all().order_by("-points")
+    return HttpResponse(json.dumps(list(rankings.values("username", "points", "positivity"))))
