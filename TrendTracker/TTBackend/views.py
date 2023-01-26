c339ad2d2f81399c5992ba351eea4a620f14362f
clydejallorina <clydejallorina@gmail.com>
2023-01-26 18:05:51 +0800

    products view

--- TrendTracker/TTBackend/views.py
+++ TrendTracker/TTBackend/views.py
@@ -7,8 +7,21 @@ import json
 
 ### DUMMY API VIEWS, REMOVE THESE WHEN WE ACTUALLY HAVE PROPER VIEWS
 def product_rankings(request):
-    test_rankings = [{"1": {"name": "TestProduct", "url": "http://testurl.com/"}}, {"2": {"name": "TestProduct2", "url": "http://testurl.com/"}}]
-    return HttpResponse(str(test_rankings))
+    rankings = Product.objects.all().order_by("-points")
+    ranks = rankings.values("name", "points", "top1post", "top1url", "top1points", "top2post", "top2url", "top2points", "top3post", "top3url", "top3points")
+    final_rankings = []
+    for rank in ranks:
+        data = {}
+        data["name"] = rank["name"]
+        data["points"] = rank["points"]
+        data["posts"] = [
+            {"post": rank["top1post"], "url": rank["top1url"], "points": rank["top1points"]},
+            {"post": rank["top2post"], "url": rank["top2url"], "points": rank["top2points"]},
+            {"post": rank["top3post"], "url": rank["top3url"], "points": rank["top3points"]},
+        ]
+        final_rankings.append(data)
+    print(ranks)
+    return HttpResponse(json.dumps(final_rankings))
 
 def user_rankings(request):
     rankings = TiktokUser.objects.all().order_by("-points")
